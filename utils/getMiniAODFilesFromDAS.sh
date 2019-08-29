493545ed219ba5f7b6c907c1c5e7699b481b5745
IzaakWN <iwn_@hotmail.com>
2019-08-29 20:09:24 +0200

    add optional data set

--- utils/getMiniAODFilesFromDAS.sh
+++ utils/getMiniAODFilesFromDAS.sh
@@ -8,8 +8,9 @@ for d in `dasgoclient -query="dataset=/DYJetsToLL_M-50_TuneC*_13TeV-madgraphMLM-
   dasgoclient -query="dataset=$p file" | head -n2
 done
 
+DATA="SingleMuon" #"Tau"
 for p in Run2016*17Jul2018* Run2017*31Mar2018* Run2018*17Sep2018* Run2018D-PromptReco-v2; do
-  for d in `dasgoclient -query="dataset=/Tau/$p/MINIAOD"`; do
+  for d in `dasgoclient -query="dataset=/$DATA/$p/MINIAOD"`; do
     echo
     echo -e "\e[1m\e[32m$d\e[0m"
     dasgoclient -query="dataset=$d file" | head -n2
