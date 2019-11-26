8f8bb2e449539c7ab507c2e5f58c0d32e35a44db
IzaakWN <iwn_@hotmail.com>
2019-11-26 12:48:57 +0100

    add more options

--- plugins/CheckTriggers.cc
+++ plugins/CheckTriggers.cc
@@ -50,6 +50,7 @@ class TriggerChecks
     virtual void endRun(const edm::Run&, const edm::EventSetup&) override { }
     virtual void endJob() override;
     bool selectTrigger(const std::string&);
+    bool selectFilter(const std::string&);
     bool verbose_ = false;
     int nlast_ = 1; // number of last filters
     HLTConfigProvider hltConfig_;
@@ -57,65 +58,22 @@ class TriggerChecks
     std::map<std::string,std::set<std::string>> trigTables_;
     std::map<std::string,std::set<std::string>> trigFiltersAll_;
     std::map<std::string,std::map<std::string,std::set<std::string>>> trigFilters_;
-    std::vector<std::string> globalTags_ = { };
+    std::vector<std::string> checkFilters_;
+    std::vector<std::string> globalTags_;
     std::vector<std::string> trigNames_ = {
-      
-      // Single muon triggers
-      "HLT_IsoMu22",
-      "HLT_IsoMu22_eta2p1",
-      "HLT_IsoTkMu22",
-      "HLT_IsoTkMu22_eta2p1",
+      "HLT_Iso*Mu22*",
       "HLT_IsoMu24",
       "HLT_IsoMu27",
-      "HLT_Mu50",
-      "HLT_TkMu50",
-      "HLT_Mu100",
-      "HLT_OldMu100",
-      "HLT_TkMu100",
-      //"HLT_*Mu50*",
-      //"HLT_*Mu100*",
-      
-      // Electron muon triggers
-      "HLT_Ele25_eta2p1_WPTight_Gsf",
       "HLT_Ele27_WPTight_Gsf",
-      "HLT_Ele45_WPLoose_Gsf_L1JetTauSeeded",
       "HLT_Ele35_WPTight_Gsf",
-      "HLT_Ele32_WPTight_Gsf",
-      "HLT_Ele32_WPTight_Gsf_L1DoubleEG",
-      "HLT_Ele45_CaloIdVT_GsfTrkIdT_PFJet200_PFJet50",
-      "HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165",
-      //"HLT_Ele50*",
-      "HLT_Ele105_CaloIdVT_GsfTrkIdT",
-      "HLT_Ele115_CaloIdVT_GsfTrkIdT",
       "HLT_Photon175",
       "HLT_Photon200",
-      
-      // 2016
-      "HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1",
-      "HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20",
       "HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau30",
-      "HLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1",
       "HLT_IsoMu19_eta2p1_LooseIsoPFTau20",
       "HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg",
-      "HLT_DoubleMediumCombinedIsoPFTau35_Trk1_eta2p1_Reg",
-      
-      // 2017
       "HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1",
       "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1",
-      "HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg",
-      "HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg",
       "HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg",
-      
-      // 2018
-      "HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1" // data only
-      "HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTauHPS30_eta2p1_CrossL1",
-      "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1",          // data only
-      "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTauHPS27_eta2p1_CrossL1",
-      "HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg",         // data only
-      "HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg",                  // data only
-      "HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg",          // data only
-      "HLT_DoubleMediumChargedIsoPFTauHPS35_Trk1_eta2p1_Reg",
-      
     };
 };
 
@@ -129,7 +87,15 @@ TriggerChecks::TriggerChecks(const edm::ParameterSet& iConfig)
   verbose_           = iConfig.getUntrackedParameter<bool>("verbose",verbose_);
   nlast_             = std::max(iConfig.getUntrackedParameter<int>("nlast",nlast_),1);
   trigNames_         = iConfig.getUntrackedParameter<std::vector<std::string>>("triggers",trigNames_);
+  checkFilters_      = iConfig.getUntrackedParameter<std::vector<std::string>>("checkFilters",checkFilters_);
   trigTables_["All"] = { };
+  
+  std::smatch match;
+  std::regex wilcardexp("\\*");
+  for(std::size_t i=0; i<trigNames_.size(); ++i)
+    trigNames_[i] = std::regex_replace(trigNames_[i],wilcardexp,".*")+"_v";
+  for(std::size_t i=0; i<checkFilters_.size(); ++i)
+    checkFilters_[i] = std::regex_replace(checkFilters_[i],wilcardexp,".*");
 }
 
 
@@ -154,12 +120,23 @@ void TriggerChecks::beginRun(const edm::Run& iRun, const edm::EventSetup& iSetup
         const std::vector<std::string>& trignames = hltConfig_.triggerNames();
         for(auto const& trigname: trignames){
           if(!selectTrigger(trigname)) continue;
+          if(verbose_) std::cout << ">>>   \e[1m" << trigname << "\e[0m" << std::endl;
           std::vector<std::string> filters = hltConfig_.moduleLabels(trigname);
-          if(verbose_){
-            std::cout << ">>>   \e[1m" << trigname << "\e[0m" << std::endl;
+          std::string shortname  = removeVersionLabel(trigname);
+          if(checkFilters_.size()>0){
+            for(auto const& filter: filters){
+              if(selectFilter(filter)){
+                if(verbose_) std::cout << ">>>     \e[1m" << filter << "\e[0m" << std::endl;
+                std::string newfilter = "-> "+filter;
+                trigFilters[shortname].insert(newfilter);
+                trigFilters_["All"][shortname].insert(newfilter);
+              }else if(verbose_){
+                std::cout << ">>>     " << filter << std::endl;
+              }
+            }
+          }else if(verbose_){
             for(auto const& filter: filters) std::cout << ">>>     " << filter << std::endl;
           }
-          std::string shortname  = removeVersionLabel(trigname);
           if(int(filters.size())>=nlast_+1){
             for(int i=nlast_; i>=1; i--){
               std::string lastfilter = filters[filters.size()-i-1];
@@ -192,16 +169,16 @@ void TriggerChecks::endJob(){
   
   // MENUS PER GLOBAL TAG
   std::cout << "\n  " << std::string(4,'*') << " Summary of trigger menus "
-                      << std::string(24,'*') << std::endl;
+                      << std::string(28,'*') << std::endl;
   for(auto const& globalTag: globalTags_){
-    std::cout << "  *" << std::setw(52) << " " << "*" << std::endl;
-    std::cout << "  *   \e[1m" << std::left << std::setw(49) << globalTag << "\e[0m*" << std::endl;
+    std::cout << "  *" << std::setw(56) << " " << "*" << std::endl;
+    std::cout << "  *   \e[1m" << std::left << std::setw(53) << globalTag << "\e[0m*" << std::endl;
     for(auto const& tableName: trigTables_[globalTag]){
-      std::cout << "  *     " << std::left << std::setw(47) << tableName << "*" << std::endl;
+      std::cout << "  *     " << std::left << std::setw(51) << tableName << "*" << std::endl;
     }
   }
-  std::cout << "  *" << std::setw(52) << " " << "*" << std::endl;
-  std::cout << "  " << std::string(54,'*') << std::endl;
+  std::cout << "  *" << std::setw(56) << " " << "*" << std::endl;
+  std::cout << "  " << std::string(58,'*') << std::endl;
   
   // FILTERS PER TRIGGER
   std::vector<std::string> globalTagsAll(globalTags_);
@@ -239,14 +216,27 @@ bool TriggerChecks::selectTrigger(const std::string& path){
   for(auto const& trigname: trigNames_){
     if(trigname.find("*")!=std::string::npos){
       //std::string wilcardstr  = "(?<!\\.)\\*";
-      std::regex wilcardexp("\\*");
-      std::string trigexp = std::regex_replace(trigname,wilcardexp,".*")+"_v";
+      //std::regex wilcardexp("\\*");
+      //std::string trigexp = std::regex_replace(trigname,wilcardexp,".*")+"_v";
       //std::cout << ">>> " << trigexp << std::endl;
       std::smatch match;
-      std::regex pattern2(trigexp);
-      if(std::regex_search(path,match,pattern2)) return true;
+      std::regex pattern(trigname);
+      if(std::regex_search(path,match,pattern)) return true;
     }else{
-      if(path.find(trigname+"_v")!=std::string::npos) return true;
+      if(path.find(trigname)!=std::string::npos) return true;
+    }
+  }
+  return false;
+}
+
+bool TriggerChecks::selectFilter(const std::string& path){
+  for(auto const& filter: checkFilters_){
+    if(filter.find("*")!=std::string::npos){
+      std::smatch match;
+      std::regex pattern(filter);
+      if(std::regex_search(path,match,pattern)) return true;
+    }else{
+      if(path.find(filter)!=std::string::npos) return true;
     }
   }
   return false;
--- python/checkTriggers_cfg.py
+++ python/checkTriggers_cfg.py
@@ -4,71 +4,88 @@ from FWCore.ParameterSet.VarParsing import VarParsing
 options = VarParsing('analysis')
 options.register('verbose', 0,  mytype=VarParsing.varType.int)
 options.register('nlast',   1,  mytype=VarParsing.varType.int)
+options.register('year',   -1,  mytype=VarParsing.varType.int)
+options.register('dtype',   "", mytype=VarParsing.varType.string)
 options.register('trigger', "", mytype=VarParsing.varType.string)
+options.register('filter',  "", mytype=VarParsing.varType.string)
 options.parseArguments()
 director = "file:root://xrootd-cms.infn.it/"
 verbose  = options.verbose>0
 nlast    = options.nlast
+year     = options.year
+dtype    = options.dtype
 triggers = options.trigger.split(',') if options.trigger else [
-#   
-#   # SINGLE MUON
-#   "HLT_IsoMu22",
-#   "HLT_IsoMu22_eta2p1",
-#   "HLT_IsoTkMu22",
-#   "HLT_IsoTkMu22_eta2p1",
-#   "HLT_IsoMu24",
-#   "HLT_IsoMu27",
-#   "HLT_Mu50",
-#   "HLT_TkMu50",
-#   "HLT_Mu100",
-#   "HLT_OldMu100",
-#   "HLT_TkMu100",
-#   ###"HLT_*Mu50*",
-#   ###"HLT_*Mu100*",
-#   
-#   # SINGLE ELECTRON
-#   "HLT_Ele25_eta2p1_WPTight_Gsf",
-#   "HLT_Ele27_WPTight_Gsf",
-#   "HLT_Ele45_WPLoose_Gsf_L1JetTauSeeded",
-#   "HLT_Ele35_WPTight_Gsf",
-#   "HLT_Ele32_WPTight_Gsf",
-#   "HLT_Ele32_WPTight_Gsf_L1DoubleEG",
-  "HLT_Ele45_CaloIdVT_GsfTrkIdT_PFJet200_PFJet50",
-  "HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165",
-  #"HLT_Ele50*",
-  "HLT_Ele105_CaloIdVT_GsfTrkIdT",
-  "HLT_Ele115_CaloIdVT_GsfTrkIdT",
-  "HLT_Photon175",
-  "HLT_Photon200",
-#   
-#   # TAU 2016
-#   "HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1",
-#   "HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20",
-#   "HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau30",
-#   "HLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1",
-#   "HLT_IsoMu19_eta2p1_LooseIsoPFTau20",
-#   "HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg",
-#   "HLT_DoubleMediumCombinedIsoPFTau35_Trk1_eta2p1_Reg",
-#   
-#   # TAU 2017
-#   "HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1",
-#   "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1",
-#   "HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg",
-#   "HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg",
-#   "HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg",
-#   
-#   # TAU 2018
-#   "HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1" # data only
-#   "HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTauHPS30_eta2p1_CrossL1",
-#   "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1",          # data only
-#   "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTauHPS27_eta2p1_CrossL1",
-#   "HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg",         # data only
-#   "HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg",                  # data only
-#   "HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg",          # data only
-#   "HLT_DoubleMediumChargedIsoPFTauHPS35_Trk1_eta2p1_Reg",
+  
+  # SINGLE MUON
+  'HLT_IsoMu22',
+  'HLT_IsoMu22_eta2p1',
+  'HLT_IsoTkMu22',
+  'HLT_IsoTkMu22_eta2p1',
+  'HLT_IsoMu24',
+  'HLT_IsoMu27',
+  'HLT_Mu50',
+  'HLT_TkMu50',
+  'HLT_Mu100',
+  'HLT_OldMu100',
+  'HLT_TkMu100',
+  ###'HLT_*Mu50*',
+  ###'HLT_*Mu100*',
+  
+  # SINGLE ELECTRON
+  'HLT_Ele25_eta2p1_WPTight_Gsf',
+  'HLT_Ele27_WPTight_Gsf',
+  'HLT_Ele45_WPLoose_Gsf_L1JetTauSeeded',
+  'HLT_Ele35_WPTight_Gsf',
+  'HLT_Ele32_WPTight_Gsf',
+  'HLT_Ele32_WPTight_Gsf_L1DoubleEG',
+  'HLT_Ele45_CaloIdVT_GsfTrkIdT_PFJet200_PFJet50',
+  'HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165',
+  ###'HLT_Ele50*',
+  'HLT_Ele105_CaloIdVT_GsfTrkIdT',
+  'HLT_Ele115_CaloIdVT_GsfTrkIdT',
+  'HLT_Photon175',
+  'HLT_Photon200',
+  
+  # TAU 2016
+  'HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20_SingleL1',
+  'HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau20',
+  'HLT_Ele24_eta2p1_WPLoose_Gsf_LooseIsoPFTau30',
+  'HLT_IsoMu19_eta2p1_LooseIsoPFTau20_SingleL1',
+  'HLT_IsoMu19_eta2p1_LooseIsoPFTau20',
+  'HLT_DoubleMediumIsoPFTau35_Trk1_eta2p1_Reg',
+  'HLT_DoubleMediumCombinedIsoPFTau35_Trk1_eta2p1_Reg',
+  
+  # TAU 2017
+  'HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1',
+  'HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1',
+  'HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg',
+  'HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg',
+  'HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg',
+  
+  # TAU 2018
+  'HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1_CrossL1",    # data only
+  'HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTauHPS30_eta2p1_CrossL1',
+  'HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1',              # data only
+  'HLT_IsoMu20_eta2p1_LooseChargedIsoPFTauHPS27_eta2p1_CrossL1',
+  'HLT_DoubleMediumChargedIsoPFTau40_Trk1_TightID_eta2p1_Reg',             # data only
+  'HLT_DoubleTightChargedIsoPFTau40_Trk1_eta2p1_Reg',                      # data only
+  'HLT_DoubleTightChargedIsoPFTau35_Trk1_TightID_eta2p1_Reg',              # data only
+  'HLT_DoubleMediumChargedIsoPFTauHPS35_Trk1_eta2p1_Reg',
   
 ]
-
+filters = options.filter.split(',') if options.filter else [
+  'hltEgammaCandidates',
+  '*PixelMatchFilter',
+  '*RelTrkIso*Filtered0p4',
+  'hltL3cr*IsoFiltered0p09',
+  'hltL3f*IsoFiltered0p09',
+  #'hltIterL3MuonCandidates',
+  #'hltOldL3MuonCandidates',
+  'hlt*L3MuonCandidates',
+  'hltHighPtTkMuonCands',
+  'hlt*TkMuonCands',
+  'hlt*TrkMuonCands',
+]
 
 files = [
   
@@ -78,49 +95,49 @@ files = [
   director+'/store/mc/RunIIFall17MiniAODv2/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU2017RECOSIMstep_12Apr2018_94X_mc2017_realistic_v14-v1/70000/0256D125-5A44-E811-8C69-44A842CFD64D.root',
   director+'/store/mc/RunIIAutumn18MiniAOD/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/100000/08540B6C-AA39-3F49-8FFE-8771AD2A8885.root',
   
-  ## 2016 TAU datasets
-  #director+'/store/data/Run2016B/Tau/MINIAOD/17Jul2018_ver2-v1/40000/349B0F86-F78B-E811-8D9E-7CD30AD0A75C.root',
-  #director+'/store/data/Run2016C/Tau/MINIAOD/17Jul2018-v1/40000/52BAEB0A-478A-E811-8376-0CC47A4D764C.root',
-  #director+'/store/data/Run2016D/Tau/MINIAOD/17Jul2018-v1/40000/46AA66A9-708B-E811-9182-AC1F6B23C830.root',
-  #director+'/store/data/Run2016E/Tau/MINIAOD/17Jul2018-v1/00000/863BF917-368A-E811-9D0F-0242AC1C0503.root',
-  #director+'/store/data/Run2016F/Tau/MINIAOD/17Jul2018-v1/40000/BCBF3F41-988A-E811-A6B4-008CFAFBFC72.root',
-  #director+'/store/data/Run2016G/Tau/MINIAOD/17Jul2018-v1/20000/807650BA-C98B-E811-8B69-008CFA0A55E8.root',
-  #director+'/store/data/Run2016H/Tau/MINIAOD/17Jul2018-v1/80000/78EDACAC-8C8D-E811-8BF8-008CFA111354.root',
+  # 2016 TAU datasets
+  director+'/store/data/Run2016B/Tau/MINIAOD/17Jul2018_ver2-v1/40000/349B0F86-F78B-E811-8D9E-7CD30AD0A75C.root',
+  director+'/store/data/Run2016C/Tau/MINIAOD/17Jul2018-v1/40000/52BAEB0A-478A-E811-8376-0CC47A4D764C.root',
+  director+'/store/data/Run2016D/Tau/MINIAOD/17Jul2018-v1/40000/46AA66A9-708B-E811-9182-AC1F6B23C830.root',
+  director+'/store/data/Run2016E/Tau/MINIAOD/17Jul2018-v1/00000/863BF917-368A-E811-9D0F-0242AC1C0503.root',
+  director+'/store/data/Run2016F/Tau/MINIAOD/17Jul2018-v1/40000/BCBF3F41-988A-E811-A6B4-008CFAFBFC72.root',
+  director+'/store/data/Run2016G/Tau/MINIAOD/17Jul2018-v1/20000/807650BA-C98B-E811-8B69-008CFA0A55E8.root',
+  director+'/store/data/Run2016H/Tau/MINIAOD/17Jul2018-v1/80000/78EDACAC-8C8D-E811-8BF8-008CFA111354.root',
   
-  ## 2017 TAU datasets
-  #director+'/store/data/Run2017B/Tau/MINIAOD/31Mar2018-v1/90000/602B3645-3237-E811-B67B-A4BF0112BE4C.root',
-  #director+'/store/data/Run2017C/Tau/MINIAOD/31Mar2018-v1/00000/F057F44E-C637-E811-94D6-D4AE527EEA1D.root',
-  #director+'/store/data/Run2017D/Tau/MINIAOD/31Mar2018-v1/00000/6695F96F-E836-E811-BB64-008CFAF554C8.root',
-  #director+'/store/data/Run2017E/Tau/MINIAOD/31Mar2018-v1/90000/4075D591-6C37-E811-BED9-B4E10FA31F63.root',
-  #director+'/store/data/Run2017F/Tau/MINIAOD/31Mar2018-v1/30000/E45B7EAA-BB37-E811-9BD6-1866DAEA79C8.root',
+  # 2017 TAU datasets
+  director+'/store/data/Run2017B/Tau/MINIAOD/31Mar2018-v1/90000/602B3645-3237-E811-B67B-A4BF0112BE4C.root',
+  director+'/store/data/Run2017C/Tau/MINIAOD/31Mar2018-v1/00000/F057F44E-C637-E811-94D6-D4AE527EEA1D.root',
+  director+'/store/data/Run2017D/Tau/MINIAOD/31Mar2018-v1/00000/6695F96F-E836-E811-BB64-008CFAF554C8.root',
+  director+'/store/data/Run2017E/Tau/MINIAOD/31Mar2018-v1/90000/4075D591-6C37-E811-BED9-B4E10FA31F63.root',
+  director+'/store/data/Run2017F/Tau/MINIAOD/31Mar2018-v1/30000/E45B7EAA-BB37-E811-9BD6-1866DAEA79C8.root',
   
-  ## 2018 TAU datasets
-  #director+'/store/data/Run2018A/Tau/MINIAOD/17Sep2018-v1/270000/72909117-1CB7-A745-99E4-34F7728F0DC1.root',
-  #director+'/store/data/Run2018B/Tau/MINIAOD/17Sep2018-v1/100000/FB06FB19-4CC9-5F47-AEE2-D69D3089BA91.root',
-  #director+'/store/data/Run2018C/Tau/MINIAOD/17Sep2018-v1/120000/B21AD337-DD07-0943-9683-93FC5C1215DB.root',
-  #director+'/store/data/Run2018D/Tau/MINIAOD/PromptReco-v2/000/320/654/00000/FE8DA6C7-9896-E811-8CD8-FA163EA5E58D.root',
+  # 2018 TAU datasets
+  director+'/store/data/Run2018A/Tau/MINIAOD/17Sep2018-v1/270000/72909117-1CB7-A745-99E4-34F7728F0DC1.root',
+  director+'/store/data/Run2018B/Tau/MINIAOD/17Sep2018-v1/100000/FB06FB19-4CC9-5F47-AEE2-D69D3089BA91.root',
+  director+'/store/data/Run2018C/Tau/MINIAOD/17Sep2018-v1/120000/B21AD337-DD07-0943-9683-93FC5C1215DB.root',
+  director+'/store/data/Run2018D/Tau/MINIAOD/PromptReco-v2/000/320/654/00000/FE8DA6C7-9896-E811-8CD8-FA163EA5E58D.root',
   
-  ## 2016 SingleMuon datasets
-  #director+'/store/data/Run2016B/SingleMuon/MINIAOD/17Jul2018_ver2-v1/50000/9C3FC3BA-498B-E811-9C58-00259048AC9A.root',
-  #director+'/store/data/Run2016C/SingleMuon/MINIAOD/17Jul2018-v1/20000/141055DA-4D97-E811-9D21-1CB72C1B64E2.root',
-  #director+'/store/data/Run2016D/SingleMuon/MINIAOD/17Jul2018-v1/00000/F01191AD-278A-E811-8DE3-D48564594F36.root',
-  #director+'/store/data/Run2016E/SingleMuon/MINIAOD/17Jul2018-v1/00000/544A3D73-8C8A-E811-8724-001E6865A4D3.root',
-  #director+'/store/data/Run2016F/SingleMuon/MINIAOD/17Jul2018-v1/00000/BA7804F1-358A-E811-9D05-C0BFC0E56866.root',
-  #director+'/store/data/Run2016G/SingleMuon/MINIAOD/17Jul2018-v1/50000/0EA07C4F-F78F-E811-981F-3417EBE61338.root',
-  #director+'/store/data/Run2016H/SingleMuon/MINIAOD/17Jul2018-v1/40000/B6B0E662-668B-E811-9EA1-EC0D9A0B3300.root',
+  # 2016 SingleMuon datasets
+  director+'/store/data/Run2016B/SingleMuon/MINIAOD/17Jul2018_ver2-v1/50000/9C3FC3BA-498B-E811-9C58-00259048AC9A.root',
+  director+'/store/data/Run2016C/SingleMuon/MINIAOD/17Jul2018-v1/20000/141055DA-4D97-E811-9D21-1CB72C1B64E2.root',
+  director+'/store/data/Run2016D/SingleMuon/MINIAOD/17Jul2018-v1/00000/F01191AD-278A-E811-8DE3-D48564594F36.root',
+  director+'/store/data/Run2016E/SingleMuon/MINIAOD/17Jul2018-v1/00000/544A3D73-8C8A-E811-8724-001E6865A4D3.root',
+  director+'/store/data/Run2016F/SingleMuon/MINIAOD/17Jul2018-v1/00000/BA7804F1-358A-E811-9D05-C0BFC0E56866.root',
+  director+'/store/data/Run2016G/SingleMuon/MINIAOD/17Jul2018-v1/50000/0EA07C4F-F78F-E811-981F-3417EBE61338.root',
+  director+'/store/data/Run2016H/SingleMuon/MINIAOD/17Jul2018-v1/40000/B6B0E662-668B-E811-9EA1-EC0D9A0B3300.root',
   
-  ## 2017 SingleMuon datasets
-  #director+'/store/data/Run2017B/SingleMuon/MINIAOD/31Mar2018-v1/80000/54F30BE9-423C-E811-A315-0CC47A7C3410.root',
-  #director+'/store/data/Run2017C/SingleMuon/MINIAOD/31Mar2018-v1/100000/FAD1BDFD-8337-E811-9C8E-B499BAAC04AA.root',
-  #director+'/store/data/Run2017D/SingleMuon/MINIAOD/31Mar2018-v1/80000/1E703527-F436-E811-80A7-E0DB55FC1055.root',
-  #director+'/store/data/Run2017E/SingleMuon/MINIAOD/31Mar2018-v1/90000/AA13B7F5-A938-E811-82CE-0CC47A7C356A.root',
-  #director+'/store/data/Run2017F/SingleMuon/MINIAOD/31Mar2018-v1/30000/6CD24B25-CF37-E811-9183-0CC47A2B04DE.root',
+  # 2017 SingleMuon datasets
+  director+'/store/data/Run2017B/SingleMuon/MINIAOD/31Mar2018-v1/80000/54F30BE9-423C-E811-A315-0CC47A7C3410.root',
+  director+'/store/data/Run2017C/SingleMuon/MINIAOD/31Mar2018-v1/100000/FAD1BDFD-8337-E811-9C8E-B499BAAC04AA.root',
+  director+'/store/data/Run2017D/SingleMuon/MINIAOD/31Mar2018-v1/80000/1E703527-F436-E811-80A7-E0DB55FC1055.root',
+  director+'/store/data/Run2017E/SingleMuon/MINIAOD/31Mar2018-v1/90000/AA13B7F5-A938-E811-82CE-0CC47A7C356A.root',
+  director+'/store/data/Run2017F/SingleMuon/MINIAOD/31Mar2018-v1/30000/6CD24B25-CF37-E811-9183-0CC47A2B04DE.root',
   
-  ## 2018 SingleMuon datasets
-  #director+'/store/data/Run2018A/SingleMuon/MINIAOD/17Sep2018-v2/00000/11697BCC-C4AB-204B-91A9-87F952F9F2C6.root',
-  #director+'/store/data/Run2018B/SingleMuon/MINIAOD/17Sep2018-v1/100000/7FA66CD1-3158-F94A-A1E0-27BECABAC34A.root',
-  #director+'/store/data/Run2018C/SingleMuon/MINIAOD/17Sep2018-v1/110000/8DB2B7A5-F627-2144-8F8F-180A8DA0E90D.root',
-  #director+'/store/data/Run2018D/SingleMuon/MINIAOD/PromptReco-v2/000/320/569/00000/3C8C28E7-1A96-E811-BA8D-02163E012DD8.root',
+  # 2018 SingleMuon datasets
+  director+'/store/data/Run2018A/SingleMuon/MINIAOD/17Sep2018-v2/00000/11697BCC-C4AB-204B-91A9-87F952F9F2C6.root',
+  director+'/store/data/Run2018B/SingleMuon/MINIAOD/17Sep2018-v1/100000/7FA66CD1-3158-F94A-A1E0-27BECABAC34A.root',
+  director+'/store/data/Run2018C/SingleMuon/MINIAOD/17Sep2018-v1/110000/8DB2B7A5-F627-2144-8F8F-180A8DA0E90D.root',
+  director+'/store/data/Run2018D/SingleMuon/MINIAOD/PromptReco-v2/000/320/569/00000/3C8C28E7-1A96-E811-BA8D-02163E012DD8.root',
   
   # 2016 SingleElectron datasets
   director+'/store/data/Run2016B/SingleElectron/MINIAOD/17Jul2018_ver1-v1/40000/CACD4C5F-BE8B-E811-B0EB-00010100085C.root',
@@ -147,6 +164,22 @@ files = [
   director+'/store/data/Run2018D/EGamma/MINIAOD/PromptReco-v2/000/320/500/00000/703D2061-0096-E811-A12F-FA163EBDCF4F.root',
   
 ]
+if   year==2016: files = filter(lambda f: '/RunIISummer16' in f or '/Run2016' in f,files)
+elif year==2017: files = filter(lambda f: '/RunIIFall17'   in f or '/Run2017' in f,files)
+elif year==2018: files = filter(lambda f: '/RunIIAutumn'   in f or '/Run2018' in f,files)
+if   dtype=='data': files = filter(lambda f: '/store/mc/'   not in f,files)
+elif dtype=='mc':   files = filter(lambda f: '/store/data/' not in f,files)
+
+print ">>> "+'-'*70
+print ">>> year     = %s"%year
+print ">>> dtype    = '%s'"%dtype
+print ">>> verbose  = %s"%verbose
+print ">>> nlast    = %s"%nlast
+print ">>> triggers = %s"%triggers
+print ">>> filters  = %s"%filters
+print ">>> files    = [\n>>>   '%s"%("',\n>>>   '".join(files))+"'"
+print ">>> ]"
+print ">>> "+'-'*70
 
 process = cms.Process('TauPOG')
 process.load("FWCore.MessageService.MessageLogger_cfi")
@@ -161,8 +194,9 @@ process.source = cms.Source('PoolSource',
 )
 
 process.check = cms.EDAnalyzer('TriggerChecks',
-  triggers = cms.untracked.vstring(*triggers),
-  verbose  = cms.untracked.bool(verbose),
-  nlast    = cms.untracked.int32(nlast)
+  triggers     = cms.untracked.vstring(*triggers),
+  checkFilters = cms.untracked.vstring(*filters),
+  verbose      = cms.untracked.bool(verbose),
+  nlast        = cms.untracked.int32(nlast)
 )
 process.p = cms.Path(process.check)
