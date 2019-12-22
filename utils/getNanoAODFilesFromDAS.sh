71d0f2778e99e8b38956672bef1367c6703229dc
IzaakWN <iwn_@hotmail.com>
2019-12-22 02:07:16 +0100

    improve user settings

--- utils/getNanoAODFilesFromDAS.sh
+++ utils/getNanoAODFilesFromDAS.sh
@@ -2,14 +2,14 @@
 # dasgoclient -query="dataset=/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17NanoAODv5_PU2017RECOSIMstep_12Apr2018_v1-DeepTauv2p1_TauPOG-v1/USER instance=prod/phys03"
 
 MCSAMPLES="DYJetsToLL_M-50_TuneC*_13TeV-madgraphMLM-pythia8"
-DATASETS="Tau" #"SingleMuon" #"Tau"
+DATASETS="Tau" #SingleMuon" #"Tau"
 CAMPAIGNS="Run2016 Run2017 Run2018 Run2018D"
 NFILES=2
 
 while getopts c:d:m:n: option; do case "${option}" in
-  c) CAMPAIGNS=${OPTARG};;
-  d) DATASETS=${OPTARG};;
-  m) MCSAMPLES=${OPTARG};;
+  c) CAMPAIGNS=${OPTARG//,/ };;
+  d) DATASETS=${OPTARG//,/ };;
+  m) MCSAMPLES=${OPTARG//,/ };;
   n) NFILES=${OPTARG};;
 esac; done
 
